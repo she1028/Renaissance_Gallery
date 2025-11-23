b70da04aa01a3af76acb822662933797fd10874b
Shella May Cuenca <shellamaycuenca12@gmail.com>
2025-11-23 21:57:51 +0800

    temporary

---
+++ data.js
@@ -0,0 +1,88 @@
+var people = [
+    {
+        "title": "Saint George and the Dragon",
+        "artist": "Raphael",
+        "picture": "images/artwork10.jpg",
+        "year": "c. 1506",
+        "medium": "Oil on panel"
+    },
+    {
+        "title": "A Young Woman and Her Little Boy",
+        "artist": "Agnolo Bronzino",
+        "picture": "images/artwork6.jpg",
+        "year": "c. 1540",
+        "medium": "Oil on panel"
+    },
+    {
+        "title": "Titian's Schoolmaster",
+        "artist": "Giovanni Battista Moroni",
+        "picture": "images/artwork1.jpg",
+        "year": "c. 1575",
+        "medium": "Oil on canvas"
+    },
+    {
+        "title": "Saint John in the Desert",
+        "artist": "Domenico Veneziano",
+        "picture": "images/artwork3.jpg",
+        "year": "c. 1445/1450",
+        "medium": "Tempera on panel"
+    },
+    {
+        "title": "Charity",
+        "artist": "Mino da Fiesole",
+        "picture": "images/artwork7.jpg",
+        "year": "1475/1480",
+        "medium": "Marble"
+    },
+    {
+        "title": "Saint Luke",
+        "artist": "Giorgio Vasari",
+        "picture": "images/artwork5.jpg",
+        "year": "1570–1571",
+        "medium": "Oil on panel"
+    }, {
+        "title": "Monsignor della Casa",
+        "artist": "Pontormo",
+        "picture": "images/artwork2.jpg",
+        "year": "probably 1541/1544",
+        "medium": "Oil on panel"
+    },
+
+    {
+        "title": "Seated Nymph",
+        "artist": "Antico",
+        "picture": "images/artwork11.jpg",
+        "year": "1503",
+        "medium": "Bronze with mercury gilding on drapery and hair, silvering and silver foil in the eyes"
+    },{
+        "title": "Putto Poised on a Globe",
+        "artist": "Andrea del Verrocchio",
+        "picture": "images/artwork9.jpg",
+        "year": "c. 1480",
+        "medium": "Unbaked clay"
+    },
+    {
+        "title": "Madonna and Child",
+        "artist": "Donatello",
+        "picture": "images/artwork12.jpg",
+        "year": "c. 1420/1425",
+        "medium": "Painted and gilded terracotta"
+    },
+
+
+     {
+        "title": "Saint Luke",
+        "artist": "Giorgio Vasari",
+        "picture": "images/artwork4.jpg",
+        "year": "1570–1571",
+        "medium": "Oil on panel"
+    },
+
+    {
+        "title": "Summer",
+        "artist": "Jacopo Tintoretto",
+        "picture": "images/artwork13.jpg",
+        "year": "c. 1546/1548",
+        "medium": "Oil on canvas"
+    }
+];
